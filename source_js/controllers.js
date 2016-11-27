adfaa3008350a8b5c1aab22cf7c28c69b1cdc2a3
qzhang72 <qzhang72@illinois.edu>
2016-11-26 20:25:49 -0600

    services

--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,54 +1,18 @@
 var mp4Controllers = angular.module('mp4Controllers', []);
 
-// mp4Controllers.controller('HouseDetail', ['$scope', function($scope) {
-// }]);
-
-mp4Controllers.controller('FirstController', ['$scope', 'CommonData'  , function($scope, CommonData) {
-  $scope.data = "";
-   $scope.displayText = ""
-
-  $scope.setData = function(){
-    CommonData.setData($scope.data);
-    $scope.displayText = "Data set"
-
-  };
-
-}]);
-
-mp4Controllers.controller('SecondController', ['$scope', 'CommonData' , function($scope, CommonData) {
-  $scope.data = "";
-
-  $scope.getData = function(){
-    $scope.data = CommonData.getData();
-
-  };
-
-}]);
-
-
-mp4Controllers.controller('LlamaListController', ['$scope', '$http', 'Llamas', '$window' , function($scope, $http,  Llamas, $window) {
-
-  Llamas.get().success(function(data){
-    $scope.llamas = data;
-  });
-
-
-}]);
-
-mp4Controllers.controller('SettingsController', ['$scope' , '$window' , function($scope, $window) {
-  $scope.url = $window.sessionStorage.baseurl;
-
-  $scope.setUrl = function(){
-    $window.sessionStorage.baseurl = $scope.url;
-    $scope.displayText = "URL set";
-
-  };
-
-}]);
 
 mp4Controllers.constructor('HouseDetailView', ['$scope', '$window', '$routeParams', 'HousesGateway', function($scope, $window, rp, HousesGateway){
   var id = rp.id;
   HousesGateway.getOne(id).success(function(data){
     $scope.house = data.data;
+    console.log($scope.house);
+  });
+}]);
+
+mp4Controllers.constructor('UserDetailView', ['$scope', '$window', '$routeParams', 'UsersGateway', function($scope, $window, rp, UsersGateway){
+  var id = rp.id;
+  UsersGateway.getOne(id).success(function(data){
+    $scope.user = data.data;
+    console.log($scope.user);
   });
 }]);
--- source_js/services.js
+++ source_js/services.js
@@ -3,28 +3,6 @@ var mp4Services = angular.module('mp4Services', []);
 
 var baseUrl = "http://localhost:3000";
 
-mp4Services.factory('CommonData', function(){
-    var data = "";
-    return{
-        getData : function(){
-            return data;
-        },
-        setData : function(newData){
-            data = newData;
-        }
-    }
-});
-
-mp4Services.factory('Llamas', function($http, $window) {
-    return {
-        get : function() {
-            var baseUrl = $window.sessionStorage.baseurl;
-            return $http.get(baseUrl+'/api/llamas');
-        }
-    }
-});
-
-
 mp4Services.factory('HousesGateway', function($http) {
     var url = baseUrl + "/api/houses";
    return {
